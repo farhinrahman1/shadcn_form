212d5822db6cd563e5bcc11844f97978798429b9
Farhin Rahman <farhin.rahmanp@gmail.com>
2024-04-29 00:24:01 +0600

    erroe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white">
-      <span className="border-white border-2 p-2 rounded-md font-sans font-semibold">
-        ShadCN Form
-      </span>
-      <button className="bg-white text-black font-bold hover:bg-slate-600 p-2 rounded-md">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-y-10 bg-slate-900 text-white h-full">
+      <h1 className="border-black text-yellow-200 text-[1.5rem] border-2 p-2 rounded-md font-sans font-bold">
+        ShadCN
+      </h1>
+      <button className="bg-yellow-300 text-black font-semibold hover:bg-black hover:text-white p-2 font-sans rounded-md">
         Open Form
       </button>
     </main>
