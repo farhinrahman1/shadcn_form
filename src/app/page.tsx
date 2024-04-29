2c287b8235dcd652d44fb3f5a69d9322b68ebb02
Farhin Rahman <farhin.rahmanp@gmail.com>
2024-04-29 15:50:16 +0600

    shadcn form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,25 +13,70 @@ import {
 } from "@/components/ui/form";
 import { useForm } from "react-hook-form";
 import { Input } from "@/components/ui/input";
+import {
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+import { Select } from "@radix-ui/react-select";
 
-const formSchema = zod.object({
-  emailAddress: zod.string().email(),
-});
+const formSchema = zod
+  .object({
+    emailAddress: zod.string().email(),
+    password: zod.string().min(5),
+    passwordConfirm: zod.string().min(5),
+    projectType: zod.enum(["personal", "professional"]),
+    projectName: zod.string().optional(),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.passwordConfirm;
+    },
+    {
+      message: "Passwords do not match",
+      path: ["passwordConfirm"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.projectType === "professional") {
+        return data.projectName !== undefined;
+      }
+      return true;
+    },
+    {
+      message: "Project name is required for professional projects",
+      path: ["projectName"],
+    }
+  );
 
 export default function Home() {
   const form = useForm<zod.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       emailAddress: "",
+      password: "",
+      passwordConfirm: "",
+      projectName: "",
     },
   });
+  const projectType = form.watch("projectType");
 
-  const handleSubmit = () => {};
+  const handleSubmit = (values: zod.infer<typeof formSchema>) => {
+    console.log({ values });
+  };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-y-10 h-full">
+    <main className="flex min-h-screen bg-slate-900 flex-col items-center justify-center gap-y-3 h-full">
+      <h1 className="text-white font-bold font-sans text-2xl ">
+        Project Form <span className="text-sm font-sans">(shadcn)</span>
+      </h1>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(handleSubmit)}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="max-w-80 w-full gap-y-4 flex flex-col  items-center justify-center p-4 bg-white shadow-xl shadow-black rounded-md"
+        >
           <FormField
             control={form.control}
             name="emailAddress"
@@ -51,7 +96,87 @@ export default function Home() {
               );
             }}
           />
-          <button type="submit">Submit</button>
+          <FormField
+            control={form.control}
+            name="projectType"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel className="mx-1">Project Type</FormLabel>
+                  <Select onValueChange={field.onChange}>
+                    <FormControl>
+                      <SelectTrigger style={{ width: 230 }}>
+                        <SelectValue placeholder="Select project type" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="personal">Personal</SelectItem>
+                      <SelectItem value="professional">Professional</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage></FormMessage>
+                </FormItem>
+              );
+            }}
+          />
+          {projectType === "professional" && (
+            <FormField
+              control={form.control}
+              name="projectName"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Project Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Project Name" {...field} />
+                    </FormControl>
+                    <FormMessage></FormMessage>
+                  </FormItem>
+                );
+              }}
+            />
+          )}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage></FormMessage>
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            control={form.control}
+            name="passwordConfirm"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Confirm password"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage></FormMessage>
+                </FormItem>
+              );
+            }}
+          />
+
+          <button
+            className="bg-gray-900 text-white hover:bg-gray-950 p-2 rounded-md"
+            type="submit"
+          >
+            Submit
+          </button>
         </form>
       </Form>
     </main>
