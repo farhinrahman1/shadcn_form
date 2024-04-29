8c1ffacf09e760e8c951f7bcf5da5f18395d2014
Farhin Rahman <farhin.rahmanp@gmail.com>
2024-04-29 13:18:29 +0600

    email input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,59 @@
 "use client";
+import { FormProvider, useFormContext } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = zod.object({
+  emailAddress: zod.string().email(),
+});
 
 export default function Home() {
+  const form = useForm<zod.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      emailAddress: "",
+    },
+  });
+
+  const handleSubmit = () => {};
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-y-10 bg-slate-900 text-white h-full">
-      <h1 className="border-black text-yellow-200 text-[1.5rem] border-2 p-2 rounded-md font-sans font-bold">
-        ShadCN
-      </h1>
-      <button className="bg-yellow-300 text-black font-semibold hover:bg-black hover:text-white p-2 font-sans rounded-md">
-        Open Form
-      </button>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-y-10 h-full">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+          <FormField
+            control={form.control}
+            name="emailAddress"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Email Address</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Email Address"
+                      type="email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage></FormMessage>
+                </FormItem>
+              );
+            }}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </Form>
     </main>
   );
 }
